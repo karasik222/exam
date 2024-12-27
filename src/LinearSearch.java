b8912549fe4adf6e3efcceebfb8cd5ac49c41a33
Зизяк Даниил Александрович <217544@edu.fa.ru>
2024-12-27 12:54:56 +0300

    Initial commit

---
+++ src/LinearSearch.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+
+
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.print("Введите число для поиска: ");
+        int target = scanner.nextInt();
+
+        int result = linearSearch(array, target);
+
+        if (result != -1) {
+            System.out.println("Элемент найден на позиции: " + result);
+        } else {
+            System.out.println("Элемент не найден в массиве.");
+        }
+
+        scanner.close();
+    }
+}
